9da2201f170a67f155b2133087d9443e2edc7bdb
daniel-gf05 <danielfgayoso@gmail.com>
2025-12-20 22:23:23 +0100

    projects api

---
+++ src/pages/api/projectsApi.ts
@@ -0,0 +1,28 @@
+import { MongoClient } from "mongodb";
+
+// Conexión a la base de datos
+const client = new MongoClient(import.meta.env.MONGODB_URI);
+
+export async function GET() {
+
+    try {
+
+        // Establecer conexión
+        await client.connect()
+
+        // Apuntar a la base de datos y colección
+        const database = client.db('Portfolio');
+        const projectsCollection = database.collection('projects');
+
+        // Ejecuto la consulta y lo convierto en un js array
+        const projects = await projectsCollection.find({}).toArray();
+        console.log("Data obtained:", projects)
+
+        return new Response(JSON.stringify(projects), {
+            status: 200,
+            headers: { "Content-Type": "application/json" }
+        })
+    } catch (error) {
+        console.log("Error en api: ", error)
+    }
+}
